d828fdf3301e1987e07996ef92d608ab36412d4b
dexon03 <dima0958757803@gmail.com>
2024-05-03 12:52:33 +0300

    added daily rule, refactor, added migrations and statistics

--- UkrChatBot/Database/ApplicationDbContext.cs
+++ UkrChatBot/Database/ApplicationDbContext.cs
@@ -5,6 +5,15 @@ namespace UkrChatBot.Database;
 
 public class ApplicationDbContext : DbContext
 {
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Handler>().HasData(
+            new Handler { Id = 1, Title = "start", ClickedCount = 0 },
+            new Handler { Id = 2, Title = "categories", ClickedCount = 0 },
+            new Handler { Id = 3, Title = "dailyrule", ClickedCount = 0 }
+        );
+    }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseNpgsql("Host=localhost;Database=UkrChatBotDb;Username=postgres;Password=postgres");
---
+++ UkrChatBot/Migrations/20240503094438_Initial.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace UkrChatBot.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Handlers",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    ClickedCount = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Handlers", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Handlers",
+                columns: new[] { "Id", "ClickedCount", "Title" },
+                values: new object[,]
+                {
+                    { 1L, 0L, "start" },
+                    { 2L, 0L, "categories" },
+                    { 3L, 0L, "dailyrule" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Handlers");
+        }
+    }
+}
